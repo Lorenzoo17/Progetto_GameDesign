57f836059d62f035068b427a0b8895bf12391d76
alebonch <alebonciani@gmail.com>
2026-05-06 12:10:42 +0200

    tommaso

--- Assets/Scripts/General/HealthSystem.cs
+++ Assets/Scripts/General/HealthSystem.cs
@@ -15,18 +15,40 @@ public class HealthSystem : MonoBehaviour, IDamageable {
     [SerializeField] private float maxHealth;
     public float CurrentHealth { get; private set; }
 
+    [SerializeField] private float defense;
     public event EventHandler<DamageEventArgs> OnDamageTaken;
 
     private void Awake() {
         CurrentHealth = maxHealth;
+        defense = 0;
     }
 
     public void TakeDamage(DamageInfo damageInfo) {
-        CurrentHealth -= damageInfo.Damage;
-        OnDamageTaken?.Invoke(this, new DamageEventArgs(damageInfo.Damage, damageInfo.Direction));
+        DamageInfo modifiedDamageInfo = TakeDamageLol(damageInfo);
+        CurrentHealth -= modifiedDamageInfo.Damage;
+        OnDamageTaken?.Invoke(this, new DamageEventArgs(modifiedDamageInfo.Damage, modifiedDamageInfo.Direction));
 
         if (CurrentHealth <= 0) {
             Destroy(gameObject);
         }
+    }
+
+    private DamageInfo TakeDamageLol(DamageInfo damageInfo) {
+        DamageInfo modifiedDamageInfo = new DamageInfo(
+            damageInfo.Damage * (100/(100 + defense)), // formula danno ridotto da armor
+            damageInfo.Direction,
+            damageInfo.Source,
+            damageInfo.SourceFaction
+        );
+        return modifiedDamageInfo;
+    }
+    private DamageInfo TakeDamageOther(DamageInfo damageInfo) {
+        DamageInfo modifiedDamageInfo = new DamageInfo(
+            Math.Log2(damageInfo.Damage) * (100/(100 + defense)), // formula danno ridotto da armor
+            damageInfo.Direction,
+            damageInfo.Source,
+            damageInfo.SourceFaction
+        );
+        return damageInfo;
     }
 }
