729340c5f6d24b296c1561b1c196dccb61ecb6c8
TUMOLO FEDERICO s349520 <s349540@studenti.polito.it>
2026-05-14 14:47:59 +0200

    added male aoe mutagen scriptable object

---
+++ Assets/Scripts/ScriptableObject/Mutagen/AOEMutagenSO.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAOEData", menuName = "Combat/AOE Data")]
+public class AOEMutagenSO : MutagenSO
+{
+    [Header("Visual & Sound")]
+    public ShakeDataType shakeType = ShakeDataType.MeleeAttack;
+    public SoundID soundEffect = SoundID.PlayerAttack;
+
+    [Header("Stats")]
+    public float radius = 3f;            // Raggio dell'esplosione
+    public float damageMultiplier = 2f;  // Moltiplicatore del danno base del player
+    public float cooldown = 5f;          // Tempo di attesa tra un attacco e l'altro
+
+
+    public override void Activate(Player player, MutagenInstance instance)
+    {
+        player.playerAttack.triggerAOE(player);
+    }
+
+    public override void Tick(
+        Player player,
+        MutagenInstance instance,
+        float deltaTime)
+    {
+
+    }
+
+    public override void Deactivate(
+        Player player,
+        MutagenInstance instance)
+    {
+
+    }
+}
