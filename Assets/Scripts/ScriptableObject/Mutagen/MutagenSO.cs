adff8afc1f46f38dba8a3a1cb3cd83aa179b3b81
TUMOLO FEDERICO s349520 <s349540@studenti.polito.it>
2026-05-15 03:20:10 +0200

    Added world items to equip mutagens

--- Assets/Scripts/ScriptableObject/Mutagen/MutagenSO.cs
+++ Assets/Scripts/ScriptableObject/Mutagen/MutagenSO.cs
@@ -14,7 +14,7 @@ public abstract class MutagenSO : ScriptableObject
     public float duration;
     public GameObject animationEffect;
 
-    public abstract bool Activate(Player player, MutagenInstance instance);
+    public abstract void Activate(Player player, MutagenInstance instance);
 
     public abstract void Tick(Player player, MutagenInstance instance, float deltaTime);
 
