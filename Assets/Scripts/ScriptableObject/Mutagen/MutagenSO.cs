0e94ae3cd1ead813c04d220bb9912f55be63b038
TUMOLO FEDERICO s349520 <s349540@studenti.polito.it>
2026-06-03 01:27:51 +0200

    Fixed  UI positions

--- Assets/Scripts/ScriptableObject/Mutagen/MutagenSO.cs
+++ Assets/Scripts/ScriptableObject/Mutagen/MutagenSO.cs
@@ -27,11 +27,6 @@ public abstract class MutagenSO : ScriptableObject
 
     public abstract void Deactivate(Player player, MutagenInstance instance);
 
-    /// <summary>
-    /// Restituisce la durata base dell'effetto per un nemico specifico.
-    /// Il nemico applicherà il suo moltiplicatore personalizzato.
-    /// Sovrascrivi questo metodo se il mutagen necessita di logica personalizzata.
-    /// </summary>
     public virtual float GetEnemyEffectDuration(Enemy enemy)
     {
         return duration;
